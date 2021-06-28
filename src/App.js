c3ed638123c22067d82868b00b6bf9c5e386bc23
onas1 <80133923+onas1@users.noreply.github.com>
2021-06-28 08:48:03 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
+import React, { Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      monsters: [
+        {
+          name: 'Frankestine',
+
+          id: 'asrc1'
+        },
+        {
+          name: 'Dracula',
+          id: 'asrc2'
+        },
+        {
+          name: 'Zombie',
+          id: 'asrc3'
+        }
+
+      ]
+    };
+  }
+  render() 
+    {
+      return (
+        <div className="App">
+        {
+          this.state.monsters.map(monster =>(<h1 key={monster.id}>{monster.name}</h1>))
+        }
+        </div>
+      );
+      
+  }
 }
 
 export default App;

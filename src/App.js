d0299f5636a91085507758b19043bedda46a31a5
onas1 <80133923+onas1@users.noreply.github.com>
2021-06-28 23:17:52 +0100

    adding files for github pages

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
 import React, { Component} from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import {SearchBox} from './components/search-box/search-box.component'
 
-class App extends Component {
+
+class App extends Component
+ {
   constructor() {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: 'Frankestine',
-
-          id: 'asrc1'
-        },
-        {
-          name: 'Dracula',
-          id: 'asrc2'
-        },
-        {
-          name: 'Zombie',
-          id: 'asrc3'
-        }
-
-      ]
+      monsters: [],
+      searchField:''
     };
+
   }
-  render() 
-    {
-      return (
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response =>response.json())
+    .then(users =>this.setState({monsters: users}));
+  }
+
+handleChange = e => {
+  this.setState({searchField: e.target.value});
+}
+
+  render(){
+const {monsters, searchField} = this.state;
+const filteredMonsters =monsters.filter(monster => 
+  monster.name.toLowerCase().includes(searchField.toLowerCase())
+   )
+
+      return(
         <div className="App">
-        {
-          this.state.monsters.map(monster =>(<h1 key={monster.id}>{monster.name}</h1>))
-        }
+          <h1>Monsters Rolodex</h1>
+            <SearchBox
+                placeholder='search monsters'
+                handleChange = {this.handleChange}
+            />
+       
+          <CardList monsters={filteredMonsters} />
         </div>
       );
-      
   }
 }
 
